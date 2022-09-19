3382694eab14fbfd7ffa2732b68bc4f4cbe769b3
folu <f0lu@hotmail.co.uk>
2022-09-19 22:50:55 +0100

    basic scan

--- CheckoutKata/Checkout.cs
+++ CheckoutKata/Checkout.cs
@@ -8,23 +8,33 @@ namespace CheckoutKata
 {
     public class Checkout
     {
-        //catalogue
-        //basket
-        public Checkout()
+        private readonly IEnumerable<Item> catalog;
+        private readonly List<Item> basket;
+
+        public Checkout(IEnumerable<Item> catalog)
         {
-            
+            this.catalog = catalog;
         }
 
         public decimal Total()
         {
+            decimal total = 0;
             //calculate total price for all items in the basket
-            return 0m;
+            total = basket.Sum(x => Convert.ToDecimal(x.Price));
+            return total;
         }
 
-        public void Scan(Item item)
+        public void Scan(string sku)
         {
             // items will be scanned, valided against catalogue before adding to basket
-
+            if (!string.IsNullOrEmpty(sku))
+            {
+                var fullItem = catalog.Single(p => p.SKU == sku);
+                if (fullItem != null)
+                {
+                    basket.Add(fullItem);
+                }
+            }
         }
     }
 }
---
+++ CheckoutKata/ICheckout.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata
+{
+    public interface ICheckout
+    {
+        public void Scan(Item item);
+        public decimal Total();
+    }
+}
